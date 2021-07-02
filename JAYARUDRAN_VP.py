a6dbed065fdcb6255327420aef9d0bcd47e75f30
jayarudran <56425573+jayarudran@users.noreply.github.com>
2021-07-02 16:55:11 +0530

    Add files via upload

---
+++ JAYARUDRAN_VP.py
@@ -0,0 +1,8 @@
+#JAYARUDRAN V - P BATCH 2 - REMAINDER MESSAGE
+#python program to print a remainder message after 1 minute
+# importing time package
+import time
+# creating a time delay of 60 seconds
+time.sleep(60)
+#printing "TIMES UP " after delay OF 1 MINUTE
+print("times up")
