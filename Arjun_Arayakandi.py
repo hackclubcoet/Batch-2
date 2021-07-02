a03ce3df54a3a5d61c86b1730145a5383d6fc357
arjunajith250306 <84308036+arjunajith250306@users.noreply.github.com>
2021-07-02 12:06:15 +0530

    Add files via upload

---
+++ Arjun_Arayakandi.py
@@ -0,0 +1,25 @@
+#Arjun Arayakandi-2-stone paper scissors
+import random
+
+player_action = input("Enter a choice (rock, paper, scissors): ")
+actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(actions)
+print(f"\nYou chose {player_action}, computer chose {computer_action}.\n")
+
+if player_action == computer_action:
+    print("Both players selected {}. It's a tie ".format(player_action))
+elif player_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win")
+    else:
+        print("Paper covers rock! computer wins")
+elif player_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win")
+    else:
+        print("Scissors cuts paper! computer wins")
+elif player_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win")
+    else:
+        print("Rock smashes scissors! computer wins")
