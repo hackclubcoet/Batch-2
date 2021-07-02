c46b3143f1047c5adde6c06d3be556837f3f1893
arjunkosu <84308287+arjunkosu@users.noreply.github.com>
2021-07-02 19:45:29 +0530

    Update Arjun_Ravindran.py

--- Arjun_Ravindran.py
+++ Arjun_Ravindran.py
@@ -1,15 +1,15 @@
-#Body Mass Index
-height = float(input("Enter the height in m : "))
-weight = int(input("Enter the weight in Kg : "))
-a = weight/(height**2)
+# Arjun Ravindran,Batch 2,Body Mass Index
+height = float(input("Enter the height in m : ")) # Enter the height 
+weight = int(input("Enter the weight in Kg : "))  # Enter the weight
+a = weight/(height**2) # bmi formula
 bmi = round(a)
-if bmi<=18.5:
+if bmi<=18.5: # if bmi is less than 18.5 and then print
      print("Underweight")
-elif 18.5 < bmi <=25:
+elif 18.5 < bmi <=25: # if bmi is between 18.5 and 25 and then print
     print("Normal Weight")
-elif 25 < bmi <=30:
+elif 25 < bmi <=30: # if bmi is between 25 and 30 and then print
     print("Slightly Overweight")
-elif 30 < bmi <=35:
+elif 30 < bmi <=35 # if  bmi is between 30 and 35
     print("Obese")
-else:
+else: # if the above statements is wrong and then print
     print("Clinically Obses")
